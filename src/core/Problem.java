817c2fc60b316349d31e8ea8e8c043c75c44846a
徐驰 <mianyxc@163.com>
2013-05-05 20:31:19 +0800

    定义Problem类
    ADD: 定义了Problem类和Point类，能够根据需求随机生成TSP问题所需的数据。

---
+++ src/core/Problem.java
@@ -0,0 +1,38 @@
+package core;
+
+//Problem类定义了一个TSP所需要的信息，即点集和距离矩阵
+public class Problem {
+	public int num;
+	public Point[] points;
+	public double[][] distance;
+	
+	//构造函数，根据点的数目和在坐标系中的范围，随机生成TSP
+	public Problem(int num, double xl, double xu, double yl, double yu) {
+		this.num = num;
+		generatePoints(xl, xu, yl, yu);
+		getDistanceMatrix();
+	}
+	
+	//随机生成点集
+	private void generatePoints(double xl, double xu, double yl, double yu) {
+		points = new Point[num];
+		for(int i = 0; i < num; i++) {
+			points[i] = new Point(xl, xu, yl, yu);
+		}
+	}
+	
+	//计算点集中每两点之间的距离并存入距离矩阵
+	private void getDistanceMatrix() {
+		distance = new double[num][num];
+		for(int i = 0; i < num; i++) {
+			for(int j = 0; j < num; j++) {
+				distance[i][j] = getDistance(points[i], points[j]);
+			}
+		}
+	}
+	
+	//返回给定两点之间的距离
+	private double getDistance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+	}
+}
---
+++ src/core/TSP.java
@@ -0,0 +1,8 @@
+package core;
+
+public class TSP {
+	Problem problem;
+	Solution optimalSolution;
+	Solution playerSolution;
+	
+}
